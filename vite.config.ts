3fe86e807c89acd607a85a7430e32fb526c1d92d
doctor_ew <doctor_ew@protonmail.com>
2025-07-31 21:29:03 -0400

    pages troubleshooting

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/personal-site/',
+  // Use relative paths for local serving and building
+  base: './',
 })
